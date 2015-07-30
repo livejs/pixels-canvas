975faed2181f00a8713dde7085300c98f90d5d59
Michael Williams <dinosaur@riseup.net>
2015-07-31 02:29:05 +1200

    use pixel-to-css-color module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var genfun = require('generate-function')
+var colorStyles = require('pixel-to-css-color')
 
 module.exports = pixelsToCanvas
 
@@ -8,7 +9,11 @@ function pixelsToCanvas (canvas) {
   function compileRender (ctx, format, shape) {
     var width = canvas.width / shape[0]
     var height = canvas.height / shape[1]
-    var colorStyle = compileColorStyle(format)
+    var colorStyle = colorStyles[format]
+
+    if (colorStyle == null) {
+      throw new Error('2dpixels-canvas: unsupported color format: ', format)
+    }
 
     var render = genfun()
       ('function (x, y, color) {')
@@ -31,37 +36,4 @@ function pixelsToCanvas (canvas) {
       }
     }
   }
-}
-
-function compileColorStyle (format) {
-  var colorStyle
-  if (format === 'keyword') {
-    colorStyle = genfun()
-      ('function (color) {')
-        ('return color.get(0)')
-      ('}')
-  } else if (format === 'rgba') {
-    colorStyle = genfun()
-      ('function (color) {')
-        ('return "rgb("+ ~~color.get(0) + "," + ~~color.get(1) + "," + ~~color.get(2) + ")"')
-      ('}')
-  } else if (format === 'rgba') {
-    colorStyle = genfun()
-      ('function (color) {')
-        ('return "rgba("+ ~~color.get(0) + "," + ~~color.get(1) + "," + ~~color.get(2)) + "," + color.get(3) + ")"')
-      ('}')
-  } else if (format === 'hsl') {
-    colorStyle = genfun()
-      ('function (color) {')
-        ('return "hsl("+ color.get(0) + "," + color.get(1) + "%," + color.get(2) + "%)"')
-      ('}')
-  } else if (format === 'hsla') {
-    colorStyle = genfun()
-      ('function (color) {')
-        ('return "hsl("+ color.get(0) + "," + color.get(1) + "%," + color.get(2) + "%," + color.get(3) + ")"')
-      ('}')
-  } else {
-    throw new Error('2dpixels-canvas: unsupported color format: ', format)
-  }
-  return colorStyle.toFunction()
 }
